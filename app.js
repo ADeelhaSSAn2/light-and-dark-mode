f038abed44f9550b1077dd2d8f3e2e5434c83f6e
Adeel Hassan <adeelhassan35033@gmail.com>
2024-02-22 00:42:08 +0500

    updated

---
+++ app.js
@@ -0,0 +1,21 @@
+Swal.fire({
+    title: "DEVELOPER ADEEL ",
+    width: 600,
+    padding: "3em",
+    color: "#716add",
+    background: "#fff url(/images/trees.png)",
+    backdrop: `
+      rgba(0,0,123,0.4)
+      url("/images/nyan-cat.gif")
+      left top
+      no-repeat
+    `
+  });
+  Swal.fire({
+    title: "DEVELOPER ADEEL!",
+    text: "A Journey of a thousand miles begins with a single step.",
+    imageUrl: "img/program.webp",
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: "Custom image"
+  });
